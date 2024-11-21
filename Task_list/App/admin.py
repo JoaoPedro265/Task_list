44828f4227156db4e254a15a7954f84ff2abba2a
João pedro oliveira <joaogrilo265@gmail.com>
2024-11-21 17:48:25 -0300

    Add_models_and_admin

--- Task_list/App/admin.py
+++ Task_list/App/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Login, Task_List
 
 # Register your models here.
+admin.site.register(Login)
+admin.site.register(Task_List)
--- Task_list/App/models.py
+++ Task_list/App/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Login(models.Model):
+    first_name=models.CharField(max_length=200,default='')
+    last_name=models.CharField(max_length=200,default='')
+    email=models.EmailField(max_length=500,default='')
+    senha=models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'first_name:{self.first_name} | email:{self.email} | senha:{self.senha}'
+    
+class Task_List(models.Model):
+    user=models.ForeignKey(Login,on_delete=models.CASCADE)
+    descrition=models.CharField(max_length=500,default='')
+    test=models.CharField(max_length=500,default='')
+    completed=models.BooleanField(default=False)
