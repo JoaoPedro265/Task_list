87213d0fe915a2df68fea5e83f0ea1c6f437afaf
João pedro oliveira <joaogrilo265@gmail.com>
2025-01-02 15:50:49 -0300

    upgrading with axius

--- Task_list/AppUser/pages/PrivateRoute.jsx
+++ Task_list/AppUser/pages/PrivateRoute.jsx
@@ -1,31 +1,17 @@
-import React, { useEffect } from "react";
-import { useNavigate, Outlet } from "react-router-dom";
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
 import Cookies from "js-cookie";
-import { RenoveAccessAccToken } from "../Token/StatusToken"; // Importa a função para renovar o token
 
 function PrivateRoute() {
-    const navigate = useNavigate();
+    const accessToken = Cookies.get('access_token');
+    const refreshToken = Cookies.get('refresh_token');
 
-    useEffect(() => {
-        const checkToken = async () => {
-            let token = Cookies.get('access_token');
-            
-            // Se o token não existir, tenta renová-lo
-            if (!token) {
-                const newToken = await RenoveAccessAccToken(); // Tenta renovar o token
-                if (!newToken) {
-                    navigate('/', { replace: true }); // Redireciona para login se a renovação falhar
-                    return;
-                }
-                token = newToken; // Atualiza o token com o novo valor
-                Cookies.set('access_token', token); // Armazena o novo token no cookie
-            }
-        };
+    // Redireciona para login se os tokens ACCESS E REFRESH "não existirem"
+    if (!accessToken && !refreshToken) {
+        return <Navigate to="/" replace />
+    }
 
-        checkToken();
-    }, [navigate]); // O useEffect será reexecutado quando o navigate mudar
-
-    return <Outlet />; // Renderiza as rotas filhas (Outlets) quando o token é válido
+    return <Outlet />; // Renderiza as rotas filhas protegidas
 }
 
 export default PrivateRoute;
--- Task_list/AppUser/pages/edit.jsx
+++ Task_list/AppUser/pages/edit.jsx
@@ -1,35 +1,49 @@
 import { useState, useEffect } from "react";
 import Cookies from 'js-cookie'
+import axiosInstance from "../API/Api";
 
 export function Edit() {
   const [data, setData] = useState([])
   const [loading, setLoading] = useState(true);
   // Função para obter um cookie pelo nome
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://127.0.0.1:8000/api/')
-        if (!response.ok) { throw new Error("Erro na requisição") }
-        const data = await response.json()//converte pra json
-        setData(data)//add to data
-        console.log(data)
-        console.log(Cookies.get('access_token'))
-      } catch (error) {
-        console.error("Erro:", error);
-      }finally{
-        setLoading(false);
-      }
+
+  const fetchData = async () => {
+    try {
+      let response = await axiosInstance.get('')
+      // const response = await fetch('http://127.0.0.1:8000/api/')
+      // if (!response.ok) { throw new Error("Erro na requisição") }
+      //let data = await response.json()//converte pra json
+      console.log(response.data)
+      setData(response.data)//add to data
+    } catch (error) {
+      console.error("Erro:", error);
+    } finally {
+      setLoading(false);
     }
+  }
+
+  useEffect(() => {
     fetchData();
   }, []);
-  
-  if (loading){
+
+  if (loading) {
     return <div>Carregando...</div>;
   }
 
+  function logout(){
+    Cookies.remove('refresh_token', { path: '/' });
+    Cookies.remove('access_token', { path: '/' })
+    return
+  }
+function botton(){
+  console.log('Access Token:', Cookies.get('access_token'));
+  console.log('Refresh Token:', Cookies.get('refresh_token'));
+  return
+}
   return (
     <div>
+      <a href="" onClick={logout}>Logout</a>
       <h1>Dados da API</h1>
       <ul>
         {JSON.stringify(data)}
@@ -37,6 +51,7 @@ export function Edit() {
           <li key={item.id}>{item.username}</li>
         ))}
       </ul>
+      <button onClick={botton}>cick</button>
     </div>
 
   )
